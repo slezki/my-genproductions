69702aa99cb01eb7ed34a7c2f21585ed1d04bfdf
Alberto Sanchez Hernandez <asanchez@cern.ch>
2018-06-15 15:56:29 -0500

    for bcstar with cuep8m1

---
+++ lhe-custom-hadronization/bcstar-pythia-had-evtgen-cuep8m1.py
@@ -0,0 +1,125 @@
+import FWCore.ParameterSet.Config as cms
+
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(True),
+                         comEnergy = cms.double(13000.),
+                         ExternalDecays = cms.PSet(
+                           EvtGen130 = cms.untracked.PSet(
+                             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+                             particle_property_file = cms.FileInPath('evt_Bc_2014.pdl'),
+                             convertPythiaCodes = cms.untracked.bool(False),
+                             user_decay_embedded= cms.vstring(
+"""
+Alias B_c+_SIGNAL B_c+
+Alias B_c-_SIGNAL B_c-
+Alias myJ/psi J/psi
+ChargeConj myJ/psi myJ/psi
+ChargeConj B_c+_SIGNAL B_c-_SIGNAL
+
+Decay B_c(2S)+
+  1.0 B_c+_SIGNAL pi+ pi-  PHSP;
+Enddecay
+CDecay B_c(2S)-
+
+Decay B_c*(2S)+
+  1.0 B_c*+       pi+ pi-  PHSP;
+Enddecay
+CDecay B_c*(2S)-
+
+Decay B_c*+
+  1.0 B_c+_SIGNAL  gamma   VSP_PWAVE;
+Enddecay
+CDecay B_c*-
+
+Decay B_c+_SIGNAL
+1.0   myJ/psi  pi+         SVS;
+Enddecay
+CDecay B_c-_SIGNAL
+
+Decay myJ/psi
+1.0   mu+  mu-             PHOTOS  VLL;
+Enddecay
+
+End
+"""
+                             ),
+                             operates_on_particles = cms.vint32(100541,100543), 
+                            # list_forced_decays = cms.vstring('MyBc+','MyBc-'),  
+                           ),
+                           parameterSets = cms.vstring('EvtGen130')
+                         ),
+                         PythiaParameters = cms.PSet(
+                           pythia8CommonSettingsBlock,
+                           pythia8CUEP8M1SettingsBlock,
+                           processParameters = cms.vstring( # put below any needed pythia parameter
+#
+            '100541:new = B_c(2S)+ B_c(2S)- 1 3 0 6.8630000e+00 0.0000000e+00 6.863 6.863 0.0000000e+00',
+            '100541:isResonance = false',
+            '100541:addChannel = 1 1.0 0 541 211 -211',
+            '100541:mayDecay = off',
+#
+            '100543:new = B_c*(2S)+ B_c*(2S)- 3 3 0 6.9030000e+00 0.0000000e+00 6.903 6.903 0.0000000e+00',
+            '100543:isResonance = false',
+            '100543:addChannel = 1 1.0 0 543 211 -211',
+            '100543:mayDecay = off',
+#
+            '543:m0 = 6.34000',
+            '543:tau0 = 0.',
+            '543:mayDecay = off',
+#
+            '541:m0 = 6.2749',
+            '541:tau0 = 0.151995',
+            '541:mayDecay = off',
+#
+            'ProcessLevel:all = off',
+            'ProcessLevel:resonanceDecays = on'
+#
+                           ),
+                           parameterSets = cms.vstring('pythia8CommonSettings',
+                                                       'pythia8CUEP8M1Settings',
+                                                       'processParameters'
+                           )
+                        )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+bc2sgenfilter = cms.EDFilter("PythiaDauVFilter",
+    DaughterIDs = cms.untracked.vint32(-211, 211),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MinPt = cms.untracked.vdouble(0.4, 0.4),
+    NumberDaughters = cms.untracked.int32(2),
+    ParticleID = cms.untracked.int32(100543),
+    verbose = cms.untracked.int32(0)
+)
+
+bcgenfilter = cms.EDFilter("PythiaDauVFilter",
+    DaughterIDs = cms.untracked.vint32(211),
+    MaxEta = cms.untracked.vdouble(2.5),
+    MinEta = cms.untracked.vdouble(-2.5),
+    MinPt = cms.untracked.vdouble(1.0),
+    NumberDaughters = cms.untracked.int32(1),
+    ParticleID = cms.untracked.int32(541),
+    verbose = cms.untracked.int32(0)
+)
+
+mumugenfilter = cms.EDFilter("PythiaDauVFilter",
+    DaughterIDs = cms.untracked.vint32(-13, 13),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MinPt = cms.untracked.vdouble(3.5, 3.5),
+    MotherID = cms.untracked.int32(541),
+    NumberDaughters = cms.untracked.int32(2),
+    ParticleID = cms.untracked.int32(443),
+    verbose = cms.untracked.int32(0)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*bc2sgenfilter*bcgenfilter*mumugenfilter)
