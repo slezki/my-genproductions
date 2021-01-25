afca4cfccd14f2d1a1e12808e8f571ca5a217499
Alberto Sanchez Hernandez <asanchez@cern.ch>
2021-01-25 14:53:28 -0600

    production of jpsi in 20 to 25 GeV bin

---
+++ QuarkoniaProduccion/py8_JpsiMuMu_Pt20_25_CP5_13TeV_cfi.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(13000.0),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'Charmonium:all = on',                       # Quarkonia, MSEL=61, including feed-down as well
+            '443:onMode = off',                          # Turn off J/psi decays
+            '443:onIfMatch = 13 -13',                    # just let J/psi -> mu+ mu-
+            'PhaseSpace:pTHatMin = 15.'                  # this cut should around 10 GeV below the actual cut on Onia
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+# Filter with high pT cut on dimuon, trying to accomodate trigger requirements.
+
+mumufilter = cms.EDFilter("PythiaDauVFilter",
+        MotherID = cms.untracked.int32(0),
+        MinPt = cms.untracked.vdouble(4.0,4.0),
+        ParticleID = cms.untracked.int32(443),
+        ChargeConjugation = cms.untracked.bool(False),
+        MinEta = cms.untracked.vdouble(-1.5,-1.5),
+        MaxEta = cms.untracked.vdouble(1.5,1.5),
+        NumberDaughters = cms.untracked.int32(2),
+        DaughterIDs = cms.untracked.vint32(13, -13)
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxRapidity = cms.untracked.double(1.3),
+    MinRapidity = cms.untracked.double(-1.3),
+    MinPt = cms.untracked.double(19.5),
+    MaxPt = cms.untracked.double(25.5),
+    ParticleID = cms.untracked.int32(443)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumufilter)
